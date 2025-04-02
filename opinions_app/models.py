1032c890445e0362ca1d99b4e9008d802ff73fcf
Aleksei <galsrv@gmail.com>
2025-04-02 09:16:29 +0300

    Final

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, UTC
 
 from . import db
 
@@ -8,5 +8,20 @@ class Opinion(db.Model):
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now(UTC))
     added_by = db.Column(db.String(64))
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            text=self.text,
+            source=self.source,
+            timestamp=self.timestamp,
+            added_by=self.added_by
+        )
+
+    def from_dict(self, data):
+        for field in ['title', 'text', 'source', 'added_by']:
+            if field in data:
+                setattr(self, field, data[field])
